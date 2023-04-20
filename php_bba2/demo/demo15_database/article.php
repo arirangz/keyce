5cb0b7d6a8c2f436a01224cc6951aabe86464c00
Anthony Laplane <anthony.laplane@gmail.com>
2023-04-20 17:34:26 +0200

    db

--- php_bba2/demo/demo15_database/article.php
+++ php_bba2/demo/demo15_database/article.php
@@ -7,6 +7,20 @@ $query->bindValue(':id', $post_id, PDO::PARAM_INT);
 $query->execute();
 $post = $query->fetch(PDO::FETCH_ASSOC);
 
+if (isset($_POST["comment_form"])) {
+    echo "Comment posted";
+    $query = $pdo->prepare("INSERT INTO `comment` (`id`, `message`, `username`, `post_id`) VALUES (NULL, :message, :username, :post_id);");
+    $query->bindValue(':post_id', $post_id, PDO::PARAM_INT);
+    $query->bindValue(':message', $_POST["message"], PDO::PARAM_STR);
+    $query->bindValue(':username', $_POST["username"], PDO::PARAM_STR);
+    $query->execute();
+}
+
+$query_comments = $pdo->prepare("SELECT * FROM comment WHERE post_id = :id");
+$query_comments->bindValue(':id', $post_id, PDO::PARAM_INT);
+$query_comments->execute();
+$comments = $query_comments->fetchAll(PDO::FETCH_ASSOC);
+
 ?>
 
 
@@ -23,6 +37,28 @@ $post = $query->fetch(PDO::FETCH_ASSOC);
 <body>
     <h1><?php echo $post["title"]; ?></h1>
     <p><?php echo $post["description"]; ?></p>
+
+    <section>
+        <h2>Comments</h2>
+        <?php if ($comments) { ?>
+            <?php foreach ($comments as $key => $comment) { ?>
+                <h3><?php echo $comment["username"] ?></h3>
+                <p>Comment: <?php echo $comment["message"] ?></p>
+            <?php } ?>
+        <?php } ?>
+    </section>
+    <form method="post">
+        <h2>Leave a comment:</h2>
+        <p>
+            <label for="username">Username</label>
+            <input type="text" id="username" name="username">
+        </p>
+        <p>
+            <label for="message">Message</label>
+            <textarea name="message" id="message" cols="30" rows="5"></textarea>
+        </p>
+        <input type="submit" value="Send" name="comment_form">
+    </form>
 </body>
 
 </html>
