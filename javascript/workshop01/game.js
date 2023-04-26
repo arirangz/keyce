c843085728fa8000850e1bfff1cfb74c8da5f3d1
Anthony Laplane <anthony.laplane@gmail.com>
2023-04-26 12:12:55 +0200

    game number

--- javascript/workshop01/game.js
+++ javascript/workshop01/game.js
@@ -1,4 +1,23 @@
-let targetNumber = Math.floor((Math.random() * 100) + 1 );
+let targetNumber = Math.floor(Math.random() * 100 + 1);
+
+function checkNumber() {
+  //create a variable to store user number value
+  let userNumberValue = userNumberInput.value;
+
+  //add a if to compare userNumberValue with targetNumber
+  if (userNumberValue > targetNumber) {
+    messageDiv.textContent = userNumberValue + " is too big";
+  } else if (userNumberValue < targetNumber) {
+    messageDiv.textContent = userNumberValue + " is too small";
+  } else {
+    messageDiv.textContent = userNumberValue + " is correct";
+  }
+
+  // Empty the input
+  userNumberInput.value = "";
+  // Give back the focus to the input
+  userNumberInput.focus();
+}
 
 // for input and button
 const tryButton = document.getElementById("tryButton");
@@ -7,20 +26,11 @@ const messageDiv = document.getElementById("messageDiv");
 
 // add event listener to manage click event
 tryButton.addEventListener("click", function () {
-    //create a variable to store user number value
-    let userNumberValue = userNumberInput.value;
-    
-    //add a if to compare userNumberValue with targetNumber
-    if (userNumberValue > targetNumber) {
-        messageDiv.textContent = userNumberValue + " is too big";
-    } else if (userNumberValue < targetNumber) {
-        messageDiv.textContent = userNumberValue + " is too small";
-    } else {
-        messageDiv.textContent = userNumberValue + " is correct";
-    }
+    checkNumber();
+});
 
-    // Empty the input
-    userNumberInput.value = "";
-    // Give back the focus to the input
-    userNumberInput.focus();
+userNumberInput.addEventListener("keypress", function (event) {
+  if (event.key === "Enter") {
+    checkNumber();
+  }
 });
