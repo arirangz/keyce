27806f9d1a164e78b736dc63813f63012cb2d89f
Anthony Laplane <anthony.laplane@gmail.com>
2023-05-03 16:03:02 +0200

    tamagotchi

--- javascript/tamagotchi/assets/js/game.js
+++ javascript/tamagotchi/assets/js/game.js
@@ -2,8 +2,17 @@ const characterImagesPath = "assets/images/character/";
 const animationDuration = 4000;
 const decreaseDuration = 5000;
 const animationsImage = document.getElementById("animationsImage");
+
 const feedButton = document.getElementById("feed");
-const hungerProgress = document.getElementById("hungerBar");
+const sleepButton = document.getElementById("sleep");
+const playButton = document.getElementById("play");
+const studyButton = document.getElementById("study");
+
+const hungerBar = document.getElementById("hungerBar");
+const energyBar = document.getElementById("energyBar");
+const happinessBar = document.getElementById("happinessBar");
+const studyBar = document.getElementById("studyBar");
+
 
 let currentImage = "idle.gif";
 let imageTimeout;
@@ -22,22 +31,56 @@ function initialisation() {
   gameOverTitle.classList.add("hidden");
 }
 
+function disableButtons() {
+  const buttons = document.querySelectorAll("#actions button");
+
+  buttons.forEach((button) => {
+    button.disabled = true;
+  });
+
+}
+
+function enableButtons() {
+  const buttons = document.querySelectorAll("#actions button");
+
+  buttons.forEach((button) => {
+    button.disabled = false;
+  });
+
+}
+
 function refreshImage() {
-  //disableButtons();
+  disableButtons();
   animationsImage.src = characterImagesPath + currentImage;
   clearTimeout(imageTimeout);
   clearTimeout(decreaseTimeout);
   imageTimeout = setTimeout(function () {
     animationsImage.src = characterImagesPath + "idle.gif";
-    //enableButtons();
+    enableButtons();
     //decreaseAll();
   }, animationDuration);
 }
+
 function refreshStats() {
   if (Tamagotchi.hunger < 0) {
     Tamagotchi.hunger = 0;
   }
-  hungerProgress.value = Tamagotchi.hunger;
+  hungerBar.value = Tamagotchi.hunger;
+
+  if (Tamagotchi.energy < 0) {
+    Tamagotchi.energy = 0;
+  }
+  energyBar.value = Tamagotchi.energy;
+
+  if (Tamagotchi.happiness < 0) {
+    Tamagotchi.enhappinessergy = 0;
+  }
+  happinessBar.value = Tamagotchi.happiness;
+  
+  if (Tamagotchi.study < 0) {
+    Tamagotchi.study = 0;
+  }
+  studyBar.value = Tamagotchi.happiness;
 }
 
 function actionManagement(action, value) {
@@ -53,6 +96,15 @@ function actionManagement(action, value) {
 
 feedButton.addEventListener("click", function () {
   actionManagement("hunger", 20);
+});
+sleepButton.addEventListener("click", function () {
+  actionManagement("energy", 20);
+});
+playButton.addEventListener("click", function () {
+  actionManagement("happiness", 20);
+});
+studyButton.addEventListener("click", function () {
+  actionManagement("study", 20);
 });
 
 initialisation();
